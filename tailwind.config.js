62d9588848cdb823e4d9c9e25cc46b5471415e79
Melissa Yorulmaz <melissayorulmazz@outlook.com>
2024-05-14 14:43:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js.jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        urgent: '#d4d7ff',
+        normal: '#ffd9d4',
+      },
+    },
+  },
+  plugins: [],
+};
